95390462a30266cfa817db6beff34b79261ecc70
Bobby-Jack <pigpongproduction@gmail.com>
2024-12-06 13:26:45 +0100

    premier jet 2

--- timeline/src/components/YearMarker/YearMarker.jsx
+++ timeline/src/components/YearMarker/YearMarker.jsx
@@ -1,8 +1,16 @@
 import './YearMarker.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+
 export default function YearMarker({year="XX"}) {
+  useEffect(() => {
+    AOS.init()
+  }, [])
+
   return (
     <div className='YearMarker'>
-        <div className='YearMarkerYear'>
+        <div className='YearMarkerYear' data-aos="fade-up">
             20<span>{year}</span>
         </div>
     </div>
